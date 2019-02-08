2c88f1677fd08b0cfa84e4decf9a7ec3ab35fbb0
ysyesilyurt <yesilyurt.selim@metu.edu.tr>
2019-02-09 00:47:14 +0300

    Modifications for signup feature

--- TodoApp/urls.py
+++ TodoApp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name='index'),
     re_path(r"^todolist/(?P<listID>[0-9]+)$", views.items, name='items'),
+    re_path(r'^signup/$', views.signup, name='signup'),
 ]
